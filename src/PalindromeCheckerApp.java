e735bdc2aa88eba4725344b054fde191a52e0175
ss0834-star <ss0834@srmist.edu.in>
2026-02-23 11:18:16 +0530

    UC5:stack based

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,27 +1,31 @@
+import java.util.Stack;
+
+
 public class PalindromeCheckerApp {
-    static boolean palindrome(String word) {
-        String revword = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            revword += word.charAt(i);
-        }
-        if (word.equals(revword)) {
 
+    static boolean palindrome(String word){
+
+        String rev_word = "";
+        for(int i = word.length() - 1; i >= 0; i--){
+            rev_word += word.charAt(i);
+        }
+
+        if (word.equals(rev_word)){
             return true;
-
-
-        } else {
-
-            return false;
-
         }
 
+        else{
+            return false;
+        }
     }
 
-    static boolean palindromearr(String word) {
+    static boolean palindromeArr(String word){
+
         int start = 0;
         int end = word.length() - 1;
-        while (end >= start) {
-            if (word.charAt(start) != word.charAt(end)) {
+
+        while(end >= start){
+            if(word.charAt(start) != word.charAt(end)){
                 return false;
             }
             start++;
@@ -30,17 +34,37 @@ public class PalindromeCheckerApp {
         return true;
     }
 
-    public static void main(String[] args) {
-        float version = 2.0F;
-        System.out.println("Welcome to the Palindrome checker");
-        System.out.println("version : " + version);
-        System.out.println("System initialised successfully");
-        boolean status = palindromearr("madam");
-        if (status) {
-            System.out.println("palindrome word");
+    static boolean palindromeStack(String word){
+        Stack<Character> stack = new Stack<>();
 
-        } else {
-            System.out.println("not a palindrome");
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+
+        double version = 2.0;
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : " + version);
+        System.out.println("System initialized successfully.");
+        boolean status = palindromeArr("madam");
+        if (status){
+            System.out.println("Palindrome");
+
+        }
+        else{
+            System.out.println("not");
+
         }
     }
+
 }
