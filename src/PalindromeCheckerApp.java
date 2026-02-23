2be9e1faf8a4d54196b741bcda1f59c269e2e989
ss0834-star <ss0834@srmist.edu.in>
2026-02-23 09:53:49 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,29 @@
 public class PalindromeCheckerApp {
+    static boolean palindrome(String word){
+        String revword="";
+        for(int i= word.length()-1;i>=0;i--){
+            revword+=word.charAt(i);
+        }
+        if(word.equals(revword)){
+            System.out.println("palindrome word");
+            return true;
+
+
+        }
+        else{
+            System.out.println("not a palindrome word");
+            return false;
+
+        }
+
+    }
+
     public static void main(String[] args) {
         float version=1.0F;
         System.out.println("Welcome to the Palindrome checker");
         System.out.println("version : " +version);
         System.out.println("System initialised successfully");
+        palindrome("madam");
 
     }
 }
