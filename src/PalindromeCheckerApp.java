261d50abdfbcbd9f94021327a17446162db76df5
ss0834-star <ss0834@srmist.edu.in>
2026-03-02 10:23:01 +0530

    Change2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -89,7 +89,7 @@ public class PalindromeCheckerApp {
     public static void main(String[] args){
 
         double version = 2.0;
-        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Welcome to the Palindrome Checker Management System  ");
         System.out.println("Version : " + version);
         System.out.println("System initialized successfully.");
         boolean status = palindromeDeque("mada");
